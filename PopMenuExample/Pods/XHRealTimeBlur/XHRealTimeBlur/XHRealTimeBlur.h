ceb0bc3c8990cfcf7ea0e6ae5fa21be55ab21682
cwx1056 <wenxuanchen05@gmail.com>
2016-11-22 13:09:04 -0500

    Update pods

--- PopMenuExample/Pods/XHRealTimeBlur/XHRealTimeBlur/XHRealTimeBlur.h
+++ PopMenuExample/Pods/XHRealTimeBlur/XHRealTimeBlur/XHRealTimeBlur.h
@@ -3,12 +3,8 @@
 //  XHRealTimeBlurExample
 //
 //  Created by 曾 宪华 on 14-9-7.
-//  Copyright (c) 2014年 曾宪华 QQ群: (142557668) QQ:543413507  Gmail:xhzengAIB@gmail.com. All rights reserved.
+//  Copyright (c) 2014年 嗨，我是曾宪华(@xhzengAIB)，曾加入YY Inc.担任高级移动开发工程师，拍立秀App联合创始人，热衷于简洁、而富有理性的事物 QQ:543413507 主页:http://zengxianhua.com All rights reserved.
 //
-// ==========================================
-//  XHRealTimeBlur 蒙版  实时模糊
-// ==========================================
-
 
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
@@ -42,49 +38,37 @@ typedef NS_ENUM(NSInteger, XHBlurStyle) {
 @interface XHRealTimeBlur : UIView
 
 /**
- *  Default is XHBlurStyleTranslucent  蒙版动画状态
+ *  Default is XHBlurStyleTranslucent
  */
 @property (nonatomic, assign) XHBlurStyle blurStyle;
-/**
- *  蒙版是否显示
- */
+
 @property (nonatomic, assign) BOOL showed;
 
-// Default is 0.3  蒙版显示时间 即蒙版从进入 到 动画完成的 时间 默认 0.3 从进入到动画执行 0.3秒
+// Default is 0.3
 @property (nonatomic, assign) NSTimeInterval showDuration;
 
-// Default is 0.3  蒙版消失时间 即蒙版从消失 到 动画完成的 时间 默认 0.3 从消失到动画执行 0.3秒
+// Default is 0.3
 @property (nonatomic, assign) NSTimeInterval disMissDuration;
 
 /**
- *  是否触发点击手势，默认关闭 蒙版是否有点击事件
+ *  是否触发点击手势，默认关闭
  */
 @property (nonatomic, assign) BOOL hasTapGestureEnable;
 
-#pragma mark - 蒙版显示或者消失时， 做的操作
-#pragma mark WillShow 即将显示
 @property (nonatomic, copy) WillShowBlurViewBlcok willShowBlurViewcomplted;
-#pragma mark DidShow  已经显示
 @property (nonatomic, copy) DidShowBlurViewBlcok didShowBlurViewcompleted;
-#pragma mark WillDismiss 即将消失
+
 @property (nonatomic, copy) WillDismissBlurViewBlcok willDismissBlurViewCompleted;
-#pragma mark DidDismiss  已经消失
 @property (nonatomic, copy) DidDismissBlurViewBlcok didDismissBlurViewCompleted;
 
 
-#pragma mark - show
 - (void)showBlurViewAtView:(UIView *)currentView;
 
 - (void)showBlurViewAtViewController:(UIViewController *)currentViewContrller;
 
-#pragma mark - disMiss
 - (void)disMiss;
 
 @end
-
-// ==========================================
-//  UIView (XHRealTimeBlur)蒙版  实时模糊 分类
-// ==========================================
 
 @interface UIView (XHRealTimeBlur)
 
